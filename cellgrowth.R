c25280b7500f3114d00af604d13d6d444591ca38
saur1n <dr.saurin.parikh@gmail.com>
2019-03-28 22:18:45 -0400

    close to final?

--- cellgrowth.R
+++ cellgrowth.R
@@ -11,9 +11,13 @@ library(cellGrowth)
 source("functions/plcor.R")
 
 d <- read_excel("rawData/Exp15_19_rawData.xlsx",col_types = "numeric")
-temp <- read.table("rawData/Exp15_19.txt", header = TRUE, sep = "\t",stringsAsFactors = 0)
-title <- NULL
-title <- c('Time',temp$Sample.Name)
+sample.names <- read.table("rawData/Exp15_19.txt", header = TRUE, sep = "\t",stringsAsFactors = 0)
+
+##### INITIALIZE OUTPUT
+out = NULL
+maxgr = NULL
+dtime = NULL
+stime = NULL
 
 ##### CLEAN DATA
 d$Time <- seq(0,15*(dim(d)[1]-1),15)
@@ -25,11 +29,30 @@ df <- plcor(d,125)
 #colnames(df) <- title
 
 ##### FIT DATA
-fit = fitCellGrowth(
-  x=df$Time,
-  z=log2(df$A2)
-)
+#fit = fitCellGrowth(x=df$Time,z=log2(df$A9))
+#plot(fit, scaleX=1/(60*60), xlab="time (hours)")
+#attributes(fit)[c(3,4,5,6)]
 
-plot(fit, scaleX=1/(60*60), xlab="time (hours)")
-attributes(fit)[c(3,4,5,6)]
+#df$Time[attr(fit,"pointOfMax")]/(60*60)
+#log(2)/attr(fit,"maxGrowthRate")/60
 
+##### LOOP THROUGH ALL THE DATA
+for (i in 2:length(df)) {
+  OD = abs(matrix(unlist(df[i]), ncol = 1, byrow = TRUE))
+  fit = fitCellGrowth(x=df$Time,z=log2(OD))
+  maxgr[i-1] = attr(fit,"maxGrowthRate")/60
+  dtime[i-1] = log(2)/attr(fit,"maxGrowthRate")/60
+  stime[i-1] = df$Time[attr(fit,"pointOfMax")]/(60*60)
+}
+
+###### CREATING OUTPUT FILE
+
+out$Sample = sample.names$Sample.Name
+out$MaxGrowthRate = maxgr
+out$DoubleTime = dtime
+out$SaturationTime = stime
+
+out <- data.frame(matrix(unlist(out), nrow=length(maxgr)), stringsAsFactors=FALSE)
+colnames(out) <- c('Sample','MaxGrowthRate','DoubleTime','SaturationTime')
+
+write.csv(out,'outData/cellGrowth_T1.csv')
