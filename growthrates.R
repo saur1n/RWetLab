b35c69fda773985f581a0738a3890006a0890ac5
saur1n <dr.saurin.parikh@gmail.com>
2019-04-10 18:25:04 -0400

    update

--- growthrates.R
+++ growthrates.R
@@ -23,6 +23,8 @@ ref.name = c('GEV','GEV')
 path.out = 'outData/'
 dir.create(file.path(path.out, 'plots'), showWarnings = FALSE)
 dir.create(file.path(path.out, 'plots/growthcurves'), showWarnings = FALSE)
+dir.create(file.path(path.out, 'plots/growthcurves/growthrates'), showWarnings = FALSE)
+plot.path.out = 'outData/plots/growthcurves/growthrates/'
 
 ##### INITIALIZE OUTPUT
 out = NULL
@@ -38,15 +40,42 @@ d$Time <- d$Time*60
 ##### PATH LENGTH CORRECTION
 df <- plcor(d,125)
 
-#####
-fit <- fit_easylinear(df$Time, df$F4, h=14, quota = 1)
+##### TRY OUT FITS
+fit <- fit_easylinear(df$Time, df$F6, h=14, quota = 1)
 #summary(fit)
 #coef(fit)
 
 #par(mfrow = c(1, 2))
-plot(fit, log = "y")
+plot(fit, log = 'y')
 #plot(fit)
-log(2)/coef(fit)[[3]]/60
+log(2)/coef(fit)[[3]]
 coef(fit)[[4]]
 
+res <- fit_spline(df$Time, df$F6, optgrid = 5)
+plot(res, log = 'y')
+log(2)/coef(res)[[2]]/60
 
+##### LOOP THROUGH ALL DATA
+for (i in 2:length(df)) {
+  fit <- fit_easylinear(df$Time, df[[i]], h=14, quota = 1)
+  res <- fit_spline(df$Time, df[[i]], optgrid = 5)
+  
+  jpeg(sprintf('%splots/growthcurves/%s_%s_%s_%s_LIN_GR.png',
+               path.out,
+               expt.name,
+               sample.names$Well.Location[i-1],
+               sample.names$Sample.Name[i-1],
+               str_replace_all(sample.names$Group.Name[i-1], "[+]", "_")),
+       width=600, height=600)
+  plot(fit, log = 'y', #xlab = "Time (mins)",
+       main=sprintf('%s\n%s %s (%s)\nDoubling Time = %0.2f mins',
+                    expt.name,
+                    sample.names$Group.Name[i-1],
+                    sample.names$Sample.Name[i-1],
+                    sample.names$Well.Location[i-1],
+                    log(2)/coef(fit)[[3]]))
+  dev.off()
+  maxgr[i-1] = attr(fit0,"maxGrowthRate")/60
+  dtime[i-1] = log(2)/attr(fit0,"maxGrowthRate")/60
+  stime[i-1] = df$Time[attr(fit0,"pointOfMax")]/(60*60)
+}
