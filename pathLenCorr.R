f80c2c1353690879156f9617b7a77589ffbcd6ec
saur1n <dr.saurin.parikh@gmail.com>
2019-04-23 21:37:46 -0400

    update

--- pathLenCorr.R
+++ pathLenCorr.R
@@ -18,3 +18,19 @@ last_plot() + geom_line(aes(x=pC, y=ul125), col=2)
 
 summary(fit)$r.squared
 summary(fit)$adj.r.squared
+
+
+pr <- read_excel("rawData/Exp16_19_RawData.xlsx",col_types = "numeric")
+
+dim(pr)
+pr$Time <- seq(0,15*(dim(pr)[1]-1),15)
+pr <- pr[1:89,]
+
+for (i in 2:dim(pr)[2]) {
+  temp <- data.frame(ul125 = pr[[i]])
+  pr[[i]] <- predict(fit, temp)
+}
+
+df<- pr
+
+
