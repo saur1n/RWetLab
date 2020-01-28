6c680843ebf62d32b8b5813309b0b820f1afb7ef
saur1n <dr.saurin.parikh@gmail.com>
2020-01-28 14:23:15 -0500

    tweaks with YBR and multistrain plots

--- scripts/simple_growth.R
+++ scripts/simple_growth.R
@@ -14,43 +14,60 @@ library(stringr)
 
 path.out <- 'outData/biotek/'
 path.out.gc <- 'outData/biotek/gc/'
-expt_name <- 'BIOTEK_VAROD_200123' 
-df <- read.csv("rawData/biotek/VAROD_KIN_CLEAN_ALL.csv")
-map <- read.csv("rawData/biotek/20200122_PLATEMAP.csv")
-colnames(map) <- c('well','eod','sample','media','replicate')
+expt_name <- 'MULTISTRAIN_YBR_200127' 
+df <- read.csv("rawData/biotek/MULTISTRAIN_YBR_CLEAN_200127.csv")
+map <- read.csv("rawData/biotek/20200127_PLATEMAP.csv")
+colnames(map) <- c('well','eod','sample','media','replicate','induction')
 
-# df$Time <- seq(30,30*dim(df)[1],30)
+df$Time <- seq(30,30*dim(df)[1],30)
+
+##### BLANK CORRECTION
+blank <- NULL
+for (w in as.character(map$well[map$sample == "media"])) {
+  blank <- rbind(blank, df[,colnames(df) == w])
+}
+blank <- colMeans(blank)
+# df[4:length(df)] <- df[4:length(df)] - blank
+# 
+# df[4:length(df)] < 0
+# df[df < 0] <- NA
 
 ##### INITIALIZE OUTPUT
-out = NULL
-date = NULL
-maxgr = NULL
-dtime = NULL
-ltime = NULL
-sod = NULL
-sample = NULL
-media = NULL
+out       = NULL
+date      = NULL
+maxgr     = NULL
+dtime     = NULL
+ltime     = NULL
+sod       = NULL
+sample    = NULL
+media     = NULL
 replicate = NULL
-eod = NULL
+induction = NULL
+eod       = NULL
 
 ii <- 1
 min.od <- 0
+min.t <- 0
 for (d in unique(df$Date)) {
   temp <- df[df$Date == d,]
   for (i in 4:length(temp)) {
-    if (sum(temp[[i]] > min.od) > 5) {
-      fit0 <- fit_easylinear(temp$Time[(temp[[i]] > min.od)], temp[(temp[[i]] > min.od),i], h=5, quota = 1);
-      # jpeg(sprintf('%s%s_%s_GC.png',
-      #              path.out.gc,
-      #              expt_name,
-      #              colnames(df[i])),
-      #      width=600, height=600)
-      # plot(fit0, log = 'y',
-      #      main=sprintf('%s | %s | %0.4f \n Doubling Time = %0.2f mins',
-      #                   colnames(df[i]), map$media[map$well == colnames(df[i])], map$eod[map$well == colnames(df[i])],
-      #                   log(2)/coef(fit0)[[3]]),
-      #      ylim = c(0.1,2))
-      # dev.off()
+    # temp[i] <- temp[i] - blank
+    if (sum(temp[[i]] > min.od, na.rm = T) > 5) {
+      fit0 <- fit_easylinear(temp$Time[(temp[[i]] > min.od) & (temp$Time > min.t) & !is.na(temp[[i]])], 
+                             temp[(temp[[i]] > min.od) & (temp$Time > min.t) & !is.na(temp[[i]]),i], h=5, quota = 1);
+      jpeg(sprintf('%s%s_%s-%s_%s_GC.png',
+                   path.out.gc,
+                   expt_name,
+                   map$sample[map$well == colnames(temp[i])],
+                   as.character(map$replicate[map$well == colnames(temp[i])]),
+                   as.character(map$induction[map$well == colnames(temp[i])])),
+      width=600, height=600)
+      plot(fit0, log = 'y',
+           main=sprintf('%s | %s | %s \n Doubling Time = %0.2f mins',
+                        colnames(df[i]), map$media[map$well == colnames(df[i])], map$sample[map$well == colnames(temp[i])],
+                        log(2)/coef(fit0)[[3]]),
+           ylim = c(0.001,2.2))
+      dev.off()
       date[ii] = d
       maxgr[ii] = coef(fit0)[[3]]
       dtime[ii] = log(2)/coef(fit0)[[3]]
@@ -60,11 +77,11 @@ for (d in unique(df$Date)) {
       sample[ii] = as.character(map$sample[map$well == colnames(temp[i])])
       replicate[ii] = as.character(map$replicate[map$well == colnames(temp[i])])
       media[ii] = as.character(map$media[map$well == colnames(temp[i])])
+      induction[ii] = as.character(map$induction[map$well == colnames(temp[i])])
       ii <- ii + 1
     }
   }
 }
-
 
 out <- data.frame(Date = date,
                   MaxGR = maxgr,
@@ -74,7 +91,8 @@ out <- data.frame(Date = date,
                   LagTime = ltime,
                   Sample = sample,
                   Media = media,
-                  Replicate = replicate)
+                  Replicate = replicate,
+                  Induction = induction)
 
 # out[5] = data.frame('Sample' = colnames(df[3:i]))
 # out$MaxGR <- as.numeric(out$MaxGR)
@@ -91,31 +109,70 @@ write.csv(out, file = sprintf("%s%s_GC_Result.csv",path.out,expt_name))
 
 
 ##### MULTISTRAIN EXPTS
-# ggplot(out[out$Sample != 'blank' & out$DTime < 500 & out$DTime > 10,]) +
-#   geom_boxplot(aes(x = Sample, y = DTime, group = Sample), width = 0.5) +
-#   geom_violin(aes(x = Sample, y = DTime, group = Sample), fill = 'transparent') +
-#   geom_jitter(aes(x = Sample, y = DTime), width = 0.25) +
-#   # scale_x_discrete(breaks = c('col', 'pop'),
-#   #                  labels = c('Colony', 'Population')) +
-#   scale_y_continuous(breaks = seq(0,500,5),
-#                      minor_breaks = 0:500) +
-#   # stat_compare_means(aes(x = Group, y = DTime),
-#   #                    method = "t.test") +
-#   labs(title = expt_name,
-#        # subtitle = '4 Bio Rep | 14 Tech Rep',
-#        x = 'Samples',
-#        y = 'Doubling Time (min.)') +
-#   # coord_cartesian(ylim = c(110, 130)) +
-#   theme_linedraw() +
-#   facet_wrap(.~Media)
-# ggsave(sprintf('%s%s_DTime_Box.png',path.out,expt_name),
-#        height = 6, width = 10,
-#        dpi = 300)
+ggplot(out[out$Sample != 'media' & out$DTime < 500 & out$DTime > 10 &
+             !(out$Sample == 'YAR035C-A' & out$Replicate == 1),]) +
+  geom_boxplot(aes(x = Sample, y = DTime, group = Sample), width = 0.5) +
+  geom_violin(aes(x = Sample, y = DTime, group = Sample), fill = 'transparent') +
+  geom_jitter(aes(x = Sample, y = DTime), width = 0.25) +
+  # scale_x_discrete(breaks = c('col', 'pop'),
+  #                  labels = c('Colony', 'Population')) +
+  scale_y_continuous(breaks = seq(0,500,5),
+                     minor_breaks = 0:500) +
+  # stat_compare_means(aes(x = Group, y = DTime),
+  #                    method = "t.test") +
+  labs(title = expt_name,
+       # subtitle = '4 Bio Rep | 14 Tech Rep',
+       x = 'Samples',
+       y = 'Doubling Time (min.)') +
+  # coord_cartesian(ylim = c(110, 130)) +
+  theme_linedraw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(.~Media*Induction)
+ggsave(sprintf('%s%s_DTime_Box.png',path.out,expt_name),
+       height = 5, width = 5,
+       dpi = 300)
 
+df.melt <- melt(df, id.vars = c('Date','Time','Temp'))
+for (v in unique(df.melt$variable)) {
+  df.melt$Sample[df.melt$variable == v] <- as.character(map$sample[map$well == v])
+  df.melt$Replicate[df.melt$variable == v] <- as.character(map$replicate[map$well == v])
+  df.melt$Media[df.melt$variable == v] <- as.character(map$media[map$well == v])
+  df.melt$Induction[df.melt$variable == v] <- as.character(map$induction[map$well == v])
+}
+
+ggplot(df.melt[df.melt$Sample != 'blank' &
+                 !(df.melt$Sample == 'YAR035C-A' & df.melt$Replicate == 1),],
+       aes(x = Time/60, y = value)) +
+  geom_point(size = 0.1) +
+  stat_summary(fun.data=mean_sdl, fun.args = list(mult=1), 
+               geom="errorbar", color="red", lwd = 0.2) +
+  stat_summary(fun.y=mean, geom="point", color="red", size = 0.1) +
+  labs(title = expt_name,
+       x = 'Time',
+       y = 'Plate OD') +
+  facet_wrap(.~Sample*Induction,
+             ncol = 3) +
+  theme_linedraw()
+ggsave(sprintf('%s%s_GC_Box.png',path.out,expt_name),
+       height = 8, width = 10,
+       dpi = 300)
+
+ggplot(df.melt[df.melt$Sample != 'blank' &
+                !(df.melt$Sample == 'YAR035C-A' & df.melt$Replicate == 1),],
+      aes(x = Time/60, y = value, col = Sample)) +
+  geom_point(size = 0.4) +
+  labs(title = expt_name,
+       x = 'Time',
+       y = 'Plate OD') +
+  theme_linedraw() +
+  guides(colour = guide_legend(override.aes = list(size=5)))
+ggsave(sprintf('%s%s_GC.png',path.out,expt_name),
+       height = 5, width = 6,
+       dpi = 300)
 
 ##### VAR OD EXPTS
 ggplot(out[out$Sample != 'blank' & out$DTime < 500 & out$DTime > 10,]) +
-  geom_point(aes(x = EOD, y = DTime, col = Media, shape = Replicate), size = 3) +
+  geom_point(aes(x = EOD, y = DTime, shape = Sample, col = as.character(Replicate)), size = 3) +
   scale_y_continuous(breaks = seq(0,500,10)) +
   labs(title = expt_name,
        subtitle = sprintf('For Plates ODs > %0.2f', min.od),
@@ -123,28 +180,28 @@ ggplot(out[out$Sample != 'blank' & out$DTime < 500 & out$DTime > 10,]) +
        y = 'Doubling Time (mins.)') +
   theme_linedraw() +
   facet_wrap(.~Date*Media)
-ggsave(sprintf('%s%s_DTimeVSEOD.png',path.out,expt_name),
-       height = 10, width = 10,
-       dpi = 300)
+# ggsave(sprintf('%s%s_DTimeVSEOD.png',path.out,expt_name),
+#        height = 10, width = 10,
+#        dpi = 300)
 
 
-ggplot(out[out$Sample != 'blank' & out$DTime < 500 & out$DTime > 10 &
-             out$Date == 20200122,]) +
-  geom_abline() +
-  geom_point(aes(x = DTime,
-                 y = out$DTime[out$Sample != 'blank' & out$DTime < 500 & out$DTime > 10 &
-                           out$Date == 20200123],
-                 col = EOD, shape = Replicate),
-             size = 3) +
-  scale_color_continuous(name = 'Cuvette\nOD') +
-  theme_linedraw() +
-  labs(title = expt_name,
-       x = '01/22/2020\nDoubling Time (min.)',
-       y = '01/23/2020\nDoubling Time (min.)') +
-  facet_wrap(.~Media)
-ggsave(sprintf('%s%s_DTimes.png',path.out,expt_name),
-       height = 5, width = 10,
-       dpi = 300)
+# ggplot(out[out$Sample != 'blank' & out$DTime < 500 & out$DTime > 10 &
+#              out$Date == 20200122,]) +
+#   geom_abline() +
+#   geom_point(aes(x = DTime,
+#                  y = out$DTime[out$Sample != 'blank' & out$DTime < 500 & out$DTime > 10 &
+#                            out$Date == 20200123],
+#                  col = EOD, shape = Replicate),
+#              size = 3) +
+#   scale_color_continuous(name = 'Cuvette\nOD') +
+#   theme_linedraw() +
+#   labs(title = expt_name,
+#        x = '01/22/2020\nDoubling Time (min.)',
+#        y = '01/23/2020\nDoubling Time (min.)') +
+#   facet_wrap(.~Media)
+# ggsave(sprintf('%s%s_DTimes.png',path.out,expt_name),
+#        height = 5, width = 10,
+#        dpi = 300)
 
 # pod11 <- ggplot()
 # for (i in 3:length(df)) {
